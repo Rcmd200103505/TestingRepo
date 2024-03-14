c143c0e3dc58881bc7d656389736844d73d59d82
unknown <200103505@stu.sdu.edu.kz>
2024-03-14 20:31:01 +0600

    Добавлен список моих игр и список игр других пользователей

---
+++ Desktop/oiynlike_diploma/oiynlike/src/router/index.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import AuthorizationPage from '@/components/AuthorizationPage.vue';
+import DashboardPage from '@/components/DashboardPage.vue';
+import ProfilePage from '@/components/ProfilePage.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'AuthorizationPage',
+    component: AuthorizationPage
+  },
+  {
+    path: '/dashboard',
+    name: 'DashboardPage',
+    component: DashboardPage,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/profile',
+    name: 'ProfilePage',
+    component: ProfilePage,
+    meta: { requiresAuth: true }
+  },
+  // Другие маршруты
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem('loggedIn');
+
+  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
+    next('/');
+  } else {
+    next();
+  }
+});
+
+export default router;
